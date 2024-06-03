6b9f6629448be3e534868d980b1dcf5047c1645b
Kaleab M Nigusie <167922913+KaleabMekonen@users.noreply.github.com>
2024-06-02 23:38:35 -0700

    Initial setup.

---
+++ Initial Setup/Initial Setup/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Initial Setup
+//
+//  Created by Kaleab Mekonen on 6/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    std::cout << "Here is the solution.!\n";
+    return 0;
+}
