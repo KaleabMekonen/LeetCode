1aaf5ae2d50147866b57a9f2693771a89a42b89e
Kaleab M Nigusie <167922913+KaleabMekonen@users.noreply.github.com>
2024-06-07 01:04:02 -0700

    Init proj.

---
+++ DesignHashSet/DesignHashSet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DesignHashSet
+//
+//  Created by Kaleab Mekonen on 6/7/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
