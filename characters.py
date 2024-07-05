6a61c653b6348e81bca3f477c3043a5d476bb80f
Kaleab M Nigusie <167922913+KaleabMekonen@users.noreply.github.com>
2024-07-04 21:02:29 -0700

    initial commit.

---
+++ characters.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/description/
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+    def main(self):
+        return True
